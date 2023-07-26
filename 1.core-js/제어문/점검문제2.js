83ae1a541a25fcb9cca6512ef3fc2e7cc2b2069b
20230720 <ghkdwo34@naver.com>
2023-07-26 10:12:17 +0900

    업다운게임

--- 1.core-js/제어문/점검문제2.js
+++ 1.core-js/제어문/점검문제2.js
@@ -1,3 +1,24 @@
+const HIGH = 1;
+const MIDDLE = 2;
+const LOW = 3;
+
+let initCount;
+let countdown = initCount;
+
+let level = +prompt('난이도를 선택하세요!\n# [1. 상(3번의 기회) | 2. 중(6번의 기회) | 3. 하(10번의 기회)]');
+
+if(level === 1){
+  initCount = 3;
+}else if(level === 2){
+  initCount = 6;
+}else if(level === 3){
+  initCount = 10;
+}else{
+  alert('잘못 입력함 난이도 상 으로 감 ㅋ')
+  initCount = 3;
+}
+
+
 let secret = Math.floor(Math.random() * 100) + 1;
 while(true){
    let min = 1
