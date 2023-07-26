7d5e465984c5ae25260802686002025ed142d7ad
20230720 <ghkdwo34@naver.com>
2023-07-26 09:42:49 +0900

    숫자카운트_추가

--- 1.core-js/제어문/점검문제2.js
+++ 1.core-js/제어문/점검문제2.js
@@ -2,14 +2,19 @@ let secret = Math.floor(Math.random() * 100) + 1;
 while(true){
    let min = 1
    ,max=100;
- let answer = +prompt(`숫자를 입력하세요! [${min} ~ ${max}`);
+   let initCount = 5;
+   let countdown = initCount;
+   while(true){
+    let answer = +prompt(`숫자를 입력하세요! [${min} ~ ${max}]`);
+    countdown--;
     if(answer < min || answer > max){
         alert(`범위 안의 값을 입력하세요!`);
         continue;
     }
+    countdown--;
 
     if (secret === answer) {
-     alert(`정답입니다!`);
+     alert(`정답입니다!${initCount - countdown} 번에 맞춤 ㅋ`);
      break;
     } else if (secret > answer) {
      alert(`UP!!`);
@@ -18,5 +23,12 @@ while(true){
      alert(`DOWN!!`);
       max = answer -1;
     }
+    if(countdown === 0){
+     alert(`땡 정답은 ${secret} 거임 ㅋ`);
+     break;
+    }else{
+      alert(`신에게는 ${countdown} 번의 기회가 나왔습니다.`)
+    }
+   } 
 }
 alert(`수고하셨습니다`)
