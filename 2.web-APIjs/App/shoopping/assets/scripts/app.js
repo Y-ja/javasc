12c0501ceb216e3f501a75c8063ce066b3384d93
20230720 <ghkdwo34@naver.com>
2023-08-04 13:33:12 +0900

    실전예제-->담기버튼 이벤ㅌ, this바인딩 문제
    이벤트 핸드러 등록시 this 잘못 바인딩 된 문제 해결

--- 2.web-APIjs/App/shoopping/assets/scripts/app.js
+++ 2.web-APIjs/App/shoopping/assets/scripts/app.js
@@ -1,20 +1,3 @@
-class Dog {
-    constructor(name, age, injection, favorite) {
-      this.name = name;
-      this.age = age;
-      this.injection = injection;
-      this.favorite = favorite;
-    }
-    walkInPark() {
-      console.log(`${this.name}이 산책합니다.`);
-    }
-  }
-  const d1 = new Dog('뽀삐');
-  const d2 = new Dog('초코');
-  
-  d1.walkInPark();
-  d2.walkInPark();
-  
 // 하나의 상품 객체에 대한 설계도
 class Product {
     // 객체를 만들 때 초기값을 세팅하는 용도
@@ -35,20 +18,28 @@ class Product {
   console.log(p1);
   const p2 = new Product(
     "쩝쩝이",
-    "https://mblogthumb-phinf.pstatic.net/MjAyMDA2MjRfMTYw/MDAxNTkyOTc0MDMyMzg1.o5sHEB6oFc3dXYv0sq2S7gChprDq53kNTF8P2xabotMg.WcvwGpwKWfr1aAgGo5hx_rUxm8GU0H9dgNafD2gPiFIg.JPEG.090525_/IMG_0545.JPG?type=w800",
+    "https://www.animaxtv.co.kr/sites/animaxtv.co.kr/files/ct_character_f_primary_image/nyamnyam.jpg",
     7000,
     "쩝쩝꿀꿀박사님 점심을 추천해주세요~"
   );
   console.log(p2);
-  //한개의 li태그를 생성하는 컴포턴트 클래스 설계
-  class ProductItem{
-    constructor(product){
-        this.product = product;
+  
+  
+  // 한개의 LI태그를 생성하는 컴포넌트 클래스 설계
+  class ProductItem {
+    constructor(product) {
+      this.product = product;
     }
-    rendor(){
-        const $prod = document.createElement("li");
-        $prod.classList.add("product-item");
-        $prod.innerHTML = `
+  //담기버튼 클릭 이벤트 핸들러
+  addToCarHandler(){
+    console.log('장바구니 상품추가');
+    //상품정보
+    console.log(this.product);
+  }
+    render() {
+      const $prod = document.createElement("li");
+      $prod.classList.add("product-item");
+      $prod.innerHTML = `
           <div>
             <img src="${this.product.imageUrl}" alt="${this.product.title}">
             <div class="product-item__content">
@@ -59,34 +50,40 @@ class Product {
             </div>
           </div>
         `;
+        const $addCarBtn = $prod.querySelector('button');
+        // $addCarBtn.addEventListener('click',this.addToCarHandler.bind(this));
+        $addCarBtn.addEventListener('click',( )=> this.addToCarHandler());
         return $prod;
     }
-}
-
-  // 상품 목록에 대한 객체
-  const productList = {
-    products: [
-      p1,
-      p2,
-      new Product(
-        "햄버거",
-        "https://images.chosun.com/resizer/5jStJ5InVS4u3iHvEzB8y_tGrr8=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/HV765ADF7VF4FLG5KISDNFMUJ4.PNG",
-        16900,
-        "꽈뜨로 맥씨멈 스테카 버거~"
-      ),
-      new Product(
-        "애플워치",
-        "https://img.danawa.com/prod_img/500000/535/481/img/15481535_1.jpg?_v=20211215103510",
-        400000,
-        "APPLE watch 2세대 2022"
-      ),
-      new Product(
-        "애플망고",
-        "https://m.thegiboon.com/web/product/big/202104/ea08c22e8939ab1977487abc826b8ab8.jpg",
-        60000,
-        "맛있는 맹고~ 당장 사먹어야지~",
-      ),
-    ],
+  }
+  
+  // 한 개의 UL을 생성하는 클래스
+  class ProductList {
+    constructor() {
+      // 상품들을 모아 놓은 배열
+      this.products = [
+        p1,
+        p2,
+        new Product(
+          "햄버거",
+          "https://images.chosun.com/resizer/5jStJ5InVS4u3iHvEzB8y_tGrr8=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/HV765ADF7VF4FLG5KISDNFMUJ4.PNG",
+          16900,
+          "꽈뜨로 맥씨멈 스테카 버거~"
+        ),
+        new Product(
+          "애플워치",
+          "https://img.danawa.com/prod_img/500000/535/481/img/15481535_1.jpg?_v=20211215103510",
+          400000,
+          "APPLE watch 2세대 2022"
+        ),
+        new Product(
+          "애플망고",
+          "https://m.thegiboon.com/web/product/big/202104/ea08c22e8939ab1977487abc826b8ab8.jpg",
+          60000,
+          "맛있는 맹고~ 당장 사먹어야지~"
+        ),
+      ]
+    } // end constructor
   
     render() {
       // console.log('render!!', this);
@@ -94,16 +91,13 @@ class Product {
       const $prodList = document.createElement("ul");
       $prodList.classList.add("product-list");
       this.products.forEach((prod) => {
-        //{product : {title : ' ','imageUrl:' '}}
         const productItem = new ProductItem(prod);
         // console.log(productItem);
-        $prodList.appendChild(productItem.rendor());
+        $prodList.appendChild(productItem.render());
       });
       $app.appendChild($prodList);
-    },
-  };
-  
+    }
+  }
   // 렌더링 명령
+  const productList = new ProductList();
   productList.render();
-  
-  
